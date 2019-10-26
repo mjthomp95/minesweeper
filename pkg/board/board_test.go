2ac2ff276ee73737aa10740f5c851d3069b48e24
Micah Thompson <mjthomp95@gmail.com>
2019-10-26 01:38:33 -0400

    Updated tests for board

--- pkg/board/board_test.go
+++ pkg/board/board_test.go
@@ -12,11 +12,11 @@ func TestBlankBoard(t *testing.T) {
 		width  int
 		result Board
 	}{
-		{1, 1, Board{[][]cell{{blankCell}}, [][]OutputCell{{0}}, 1, 1}},
+		{1, 1, Board{[][]cell{{blankCell}}, [][]OutputCell{{0}}, 1, 1, 0, 1, false, false}},
 		{2, 2, Board{[][]cell{{blankCell, blankCell}, {blankCell, blankCell}},
-			[][]OutputCell{{0, 0}, {0, 0}}, 2, 2}},
+			[][]OutputCell{{0, 0}, {0, 0}}, 2, 2, 0, 4, false, false}},
 		{1, 3, Board{[][]cell{{blankCell, blankCell, blankCell}},
-			[][]OutputCell{{0, 0, 0}}, 1, 3}},
+			[][]OutputCell{{0, 0, 0}}, 1, 3, 0, 3, false, false}},
 	}
 
 	cellCheck := blankBoard(1, 1).cells
@@ -26,7 +26,7 @@ func TestBlankBoard(t *testing.T) {
 
 	for _, test := range testCases {
 		b := blankBoard(test.width, test.height)
-		if !reflect.DeepEqual(b, test.result) {
+		if !reflect.DeepEqual((*b), test.result) {
 			t.Error("Should be Equal", b, test.result)
 		}
 	}
@@ -67,6 +67,8 @@ func TestPlaceBombs(t *testing.T) {
 		} else if numMines != test.result {
 			t.Errorf("Not the correct number of bombs placed. Got: %d Expected: %d",
 				numMines, test.result)
+		} else if numMines != b.mines {
+			t.Errorf("numMines (%d) and mines in board (%d) should be equal.", numMines, b.mines)
 		}
 	}
 }
@@ -88,14 +90,14 @@ func TestNewBoard(t *testing.T) {
 	}
 
 	for _, test := range testCases {
-		b, numMines, err := NewBoard(test.width, test.height)
+		b, err := NewBoard(test.width, test.height)
 		if err == nil {
-			if counted := countBombs(b.cells); numMines != counted {
-				t.Errorf("Returned number of mines doesn't equal counted."+
-					"NumMines: %d Counted: %d", numMines, counted)
-			} else if numMines != test.result {
+			if counted := countBombs(b.cells); b.mines != counted {
+				t.Errorf("Number of mines in board doesn't equal counted. "+
+					"NumMines: %d Counted: %d", b.mines, counted)
+			} else if b.mines != test.result {
 				t.Errorf("Number of mines doesn't equal expected. Expected: %d Got: %d",
-					test.result, numMines)
+					test.result, b.mines)
 			}
 		} else {
 			t.Error("Should not be an error", err.Error())
@@ -113,11 +115,9 @@ func TestNewBoard(t *testing.T) {
 	}
 
 	for _, error := range errorCases {
-		b, numMines, err := NewBoard(error.width, error.height)
+		b, err := NewBoard(error.width, error.height)
 		if err == nil {
-			t.Errorf("Error should not be nil. Board: %v NumMines: %d", b, numMines)
-		} else if numMines != 0 {
-			t.Errorf("NumMines should be zero. Board: %v", *b)
+			t.Errorf("Error should not be nil. Board: %v NumMines: %d", b, b.mines)
 		} else if b != nil {
 			t.Errorf("Board should be nil. Board: %v", *b)
 		}
