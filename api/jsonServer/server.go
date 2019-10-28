45fb10ecb563a378cdcdd65635cbe02520c4ce05
Micah Thompson <mjthomp95@gmail.com>
2019-10-28 01:43:11 -0400

    Single board json server; currying closures; needs cleaning but works

--- api/jsonServer/server.go
+++ api/jsonServer/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,108 +11,66 @@ import (
 	mBoard "github.com/mjthomp95/minesweeper/pkg/board"
 )
 
-// TODO: Change name
+// IDEA: Create two versions of api where returning full board or just changes
 type newBoard struct {
 	Cells [][]rune `json:"cells"`
 	Mines int      `json:"mines"`
 }
 
-// TODO: change all responses from 'mark', 'choose', 'expand', 'new', and 'output' to json.
+type result struct {
+	Cells    []mBoard.Output `json:"cells"`
+	Mines    int             `json:"mines"`
+	NumCells int             `json:"numcells"`
+	Err      string          `json:"error"` //Send "Win" or "Lose" in Err when game over
+}
+
 // IDEA: use a map of ids to different boards to have multiple instances of a game.
+// IDEA: when making map, have lock for map and board. create closure to access board.
 func main() {
 	//one instance of game
 	var board *mBoard.Board
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
-		if board != nil {
-			fmt.Fprintf(w, "Game already in session")
-			return
+		if tmp := newHandler(w, r, board); tmp != nil {
+			board = tmp
 		}
+	})
 
-		if r.Method != "POST" {
-			fmt.Fprintf(w, "Only POST is supported")
-			return
-		}
+	choose := moveWrapper(func(row, col int, b **mBoard.Board) (output []mBoard.Output, err error) {
+		return (*b).Choose(row, col)
+	}, &board)
 
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprint(w, "Trouble parsing your POST form")
-			return
-		}
+	expand := moveWrapper(func(row, col int, b **mBoard.Board) (output []mBoard.Output, err error) {
+		return (*b).Expand(row, col)
+	}, &board)
 
-		heightString := r.FormValue("height")
-		widthString := r.FormValue("width")
+	mark := moveWrapper(func(row, col int, b **mBoard.Board) (output []mBoard.Output, err error) {
+		return (*b).Mark(row, col)
+	}, &board)
 
-		if heightString != "" && widthString != "" {
-			height, err := strconv.Atoi(heightString)
-			if err != nil {
-				fmt.Fprint(w, "height not a number")
-				return
+	moveHandler := func(move func(*http.Request, *result) error) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			var res result
+			if board == nil {
+				res.Err = "No game running"
+			} else {
+				move(r, &res)
+				if board != nil {
+					log.Println("\n", board.PrintBoard())
+				}
 			}
 
-			width, err := strconv.Atoi(widthString)
-			if err != nil {
-				fmt.Fprint(w, "width not a number")
-				return
-			}
-
-			tmpBoard, err := mBoard.NewBoard(height, width)
-			if err != nil {
-				fmt.Fprint(w, err.Error())
-				return
-			}
-			board = tmpBoard
-			log.Println(board.PrintBoard())
-			log.Println(board.OutputBoard())
-			resp := newBoard{Cells: board.OutputBoard(), Mines: board.GetMines()}
-			data, err := json.Marshal(resp) // TODO: Change this up
-			if err != nil {
-				fmt.Fprint(w, "couldn't marshal board")
-				return
-			}
-			log.Println(data)
 			w.Header().Set("Content-Type", "application/json")
-			w.Write(data)
-		} else {
-			fmt.Fprint(w, "Trouble getting your height and width values")
+			json.NewEncoder(w).Encode(res)
 		}
-	})
+	}
 
-	http.HandleFunc("/choose", func(w http.ResponseWriter, r *http.Request) { // IDEA: Abstract closure also
-		gameOver, win := chooseHandler(w, r, board)
-		if gameOver {
-			log.Println(board.PrintBoard())
-			board = nil
-			if win {
-				fmt.Fprint(w, "Win")
-			} else {
-				fmt.Fprint(w, "Lose")
-			}
-		}
-	})
-	http.HandleFunc("/expand", func(w http.ResponseWriter, r *http.Request) {
-		gameOver, win := expandHandler(w, r, board)
-		if gameOver {
-			log.Println("\n", board.PrintBoard())
-			board = nil
-			if win {
-				fmt.Fprint(w, "Win")
-			} else {
-				fmt.Fprint(w, "Lose")
-			}
-		}
-	})
-	http.HandleFunc("/mark", func(w http.ResponseWriter, r *http.Request) {
-		gameOver, win := markHandler(w, r, board)
-		if gameOver {
-			log.Println("\n", board.PrintBoard())
-			board = nil
-			if win {
-				fmt.Fprint(w, "Win")
-			} else {
-				fmt.Fprint(w, "Lose")
-			}
-		}
-	})
+	http.HandleFunc("/choose", moveHandler(choose))
+
+	http.HandleFunc("/expand", moveHandler(expand))
+
+	http.HandleFunc("/mark", moveHandler(mark))
+
 	http.HandleFunc("/output", func(arg1 http.ResponseWriter, arg2 *http.Request) {
 		return
 	})
@@ -125,149 +84,92 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-// IDEA: very similar handlers can I abstract and make a common handler that takes a board function to run and handle?
-func chooseHandler(w http.ResponseWriter, r *http.Request, board *mBoard.Board) (gameOver, win bool) {
-	if r.Method != "POST" {
-		fmt.Fprint(w, "Only POST is supported")
-		return
-	}
+// TODO: Change all to json
+func newHandler(w http.ResponseWriter, r *http.Request, board *mBoard.Board) *mBoard.Board {
 	if board != nil {
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprint(w, "Trouble parsing your POST form")
-			return
-		}
-		rowString := r.FormValue("row")
-		colString := r.FormValue("col")
-		if rowString != "" && colString != "" {
-			row, err := strconv.Atoi(rowString)
-			if err != nil {
-				//bad value
-				fmt.Fprint(w, "row not a number")
-				return
-			}
-			col, err := strconv.Atoi(colString)
-			if err != nil {
-				fmt.Fprint(w, "col not a number")
-				return
-			}
-			_, err = board.Choose(row, col) // TODO: Do something with output
-			switch err.(type) {
-			case nil:
-			case mBoard.GameOver:
-				gameOver = true
-				win = err.(mBoard.GameOver).Win
-				return
-			default:
-				fmt.Fprint(w, err.Error())
-			}
-			log.Println("\n", board.PrintBoard())
-			resp := newBoard{board.OutputBoard(), board.GetMines()}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(resp)
-			return
-		}
-		fmt.Fprint(w, "Trouble getting row and col values")
-		return
+		fmt.Fprintf(w, "Game already in session")
+		return nil
 	}
 
-	fmt.Fprint(w, "No game running")
+	if r.Method != "POST" {
+		fmt.Fprintf(w, "Only POST is supported")
+		return nil
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "Trouble parsing your POST form")
+		return nil
+	}
+
+	heightString := r.FormValue("height")
+	widthString := r.FormValue("width")
+
+	if heightString != "" && widthString != "" {
+		height, err := strconv.Atoi(heightString)
+		if err != nil {
+			fmt.Fprint(w, "height not a number")
+			return nil
+		}
+
+		width, err := strconv.Atoi(widthString)
+		if err != nil {
+			fmt.Fprint(w, "width not a number")
+			return nil
+		}
+
+		tmpBoard, err := mBoard.NewBoard(height, width)
+		if err != nil {
+			fmt.Fprint(w, err.Error())
+			return nil
+		}
+		resp := newBoard{Cells: tmpBoard.OutputBoard(), Mines: tmpBoard.GetMines()}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+		return tmpBoard
+	}
+	fmt.Fprint(w, "Trouble getting your height and width values")
+	return nil
+}
+
+func rowColHelper(r *http.Request) (row, col int, err error) {
+	if r.Method != "POST" {
+		err = errors.New("Only POST is supported")
+		return
+	}
+	if err = r.ParseForm(); err != nil {
+		return
+	}
+	rowString := r.FormValue("row")
+	colString := r.FormValue("col")
+	if rowString != "" && colString != "" {
+		row, err = strconv.Atoi(rowString)
+		col, err = strconv.Atoi(colString)
+	}
 	return
 }
 
-func expandHandler(w http.ResponseWriter, r *http.Request, board *mBoard.Board) (gameOver, win bool) {
-	log.Println("Expand Start")
-	if r.Method != "POST" {
-		fmt.Fprint(w, "Only POST is supported")
-		return
-	}
-	if board != nil {
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprint(w, "Trouble parsing your POST form")
+func moveWrapper(move func(int, int, **mBoard.Board) ([]mBoard.Output, error), board **mBoard.Board) func(*http.Request, *result) error {
+	// TODO: change to take board and a function that takes board to make a move if this doesn't work
+	return func(r *http.Request, res *result) (err error) {
+		row, col, rcErr := rowColHelper(r)
+		if rcErr != nil {
+			err = rcErr
 			return
 		}
-		rowString := r.FormValue("row")
-		colString := r.FormValue("col")
-		if rowString != "" && colString != "" {
-			row, err := strconv.Atoi(rowString)
-			if err != nil {
-				//bad value
-				fmt.Fprint(w, "row not a number")
-				return
+		res.Cells, err = move(row, col, board)
+		log.Println((*board).PrintBoard()) // TEMP: used for when no frontend
+		res.Mines = (*board).GetMines()
+		res.NumCells = (*board).GetNumCells()
+		switch err.(type) {
+		case nil:
+		case mBoard.GameOver:
+			(*board) = nil
+			if err.(mBoard.GameOver).Win {
+				res.Err = "Win"
+			} else {
+				res.Err = "Lose"
 			}
-			col, err := strconv.Atoi(colString)
-			if err != nil {
-				fmt.Fprint(w, "col not a number")
-				return
-			}
-			_, err = board.Expand(row, col) // TODO: Do something with output
-			switch err.(type) {
-			case nil:
-			case mBoard.GameOver:
-				gameOver = true
-				win = err.(mBoard.GameOver).Win
-				return
-			default:
-				fmt.Fprint(w, err.Error())
-			}
-			log.Println("\n", board.PrintBoard())
-			log.Println("Expand")
-			resp := newBoard{board.OutputBoard(), board.GetMines()}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(resp)
-			return
 		}
-		fmt.Fprint(w, "Trouble getting row and col values")
 		return
 	}
-
-	fmt.Fprint(w, "No game running")
-	return
-}
-
-func markHandler(w http.ResponseWriter, r *http.Request, board *mBoard.Board) (gameOver, win bool) {
-	if r.Method != "POST" {
-		fmt.Fprint(w, "Only POST is supported")
-		return
-	}
-	if board != nil {
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprint(w, "Trouble parsing your POST form")
-			return
-		}
-		rowString := r.FormValue("row")
-		colString := r.FormValue("col")
-		if rowString != "" && colString != "" {
-			row, err := strconv.Atoi(rowString)
-			if err != nil {
-				//bad value
-				fmt.Fprint(w, "row not a number")
-				return
-			}
-			col, err := strconv.Atoi(colString)
-			if err != nil {
-				fmt.Fprint(w, "col not a number")
-				return
-			}
-			_, err = board.Mark(row, col) // TODO: Do something with output
-			switch err.(type) {
-			case nil:
-			case mBoard.GameOver:
-				gameOver = true
-				win = err.(mBoard.GameOver).Win
-				return
-			default:
-				fmt.Fprint(w, err.Error())
-			}
-			log.Println("\n", board.PrintBoard())
-			resp := newBoard{board.OutputBoard(), board.GetMines()}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(resp)
-			return
-		}
-		fmt.Fprint(w, "Trouble getting row and col values")
-		return
-	}
-
-	fmt.Fprint(w, "No game running")
-	return
 }
